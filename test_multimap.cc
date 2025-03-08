214f0a2e63def2d2369712c95de7b8a6a6c22681
Pranaya Rao <132341018+pranaya99@users.noreply.github.com>
2025-03-07 22:37:54 -0800

    Update test_multimap.cc

--- test_multimap.cc
+++ test_multimap.cc
@@ -1,53 +1,40 @@
-#include <gtest/gtest.h>
-#include "multimap.h"
+#include <gtest/gtest.h>  // C++ testing library header
 
+#include <string>  // C++ system header
+#include <vector>  // C++ system header
+#include "multimap.h"
 class Multimap_OneKey_Test : public ::testing::Test {
-protected:
-  Multimap<int, int> multimap;
-  void SetUp() override {
-    multimap.Insert(1, 2);
-  }
+ protected:
+  Multimap<int, std::string> mmap;
 };
 
-TEST_F(Multimap_OneKey_Test, TestBody) {
-  std::vector<int> values = multimap.GetAll(1);  // Fetch all values for key 1
-  EXPECT_EQ(values.size(), 1);  // Expect only one value
-  EXPECT_EQ(values[0], 2);  // The value should be 2
+TEST_F(Multimap_OneKey_Test, InsertAndGet) {
+  mmap.Insert(1, "value1");
+  EXPECT_EQ(mmap.Get(1), "value1");
 }
 
 class Multimap_MultipleKeys_Test : public ::testing::Test {
-protected:
-  Multimap<int, int> multimap;
-  void SetUp() override {
-    multimap.Insert(1, 2);
-    multimap.Insert(2, 3);
-  }
+ protected:
+  Multimap<int, std::string> mmap;
 };
 
-TEST_F(Multimap_MultipleKeys_Test, TestBody) {
-  std::vector<int> values = multimap.GetAll(1);  // Fetch all values for key 1
-  EXPECT_EQ(values.size(), 1);  // Expect only one value
-  EXPECT_EQ(values[0], 2);  // The value should be 2
-
-  values = multimap.GetAll(2);  // Fetch all values for key 2
-  EXPECT_EQ(values.size(), 1);  // Expect only one value
-  EXPECT_EQ(values[0], 3);  // The value should be 3
+TEST_F(Multimap_MultipleKeys_Test, InsertAndRetrieveMultipleKeys) {
+  mmap.Insert(1, "value1");
+  mmap.Insert(2, "value2");
+  EXPECT_EQ(mmap.Get(1), "value1");
+  EXPECT_EQ(mmap.Get(2), "value2");
 }
 
 class Multimap_MultipleValues_Test : public ::testing::Test {
-protected:
-  Multimap<int, int> multimap;
-  void SetUp() override {
-    multimap.Insert(1, 1);
-    multimap.Insert(1, 2);
-    multimap.Insert(1, 3);
-  }
+ protected:
+  Multimap<int, std::string> mmap;
 };
 
-TEST_F(Multimap_MultipleValues_Test, TestBody) {
-  std::vector<int> values = multimap.GetAll(1);  // Fetch all values for key 1
-  EXPECT_EQ(values.size(), 3);  // Expect three values
-  EXPECT_EQ(values[0], 1);  // The first value should be 1
-  EXPECT_EQ(values[1], 2);  // The second value should be 2
-  EXPECT_EQ(values[2], 3);  // The third value should be 3
+TEST_F(Multimap_MultipleValues_Test, InsertMultipleValuesForSameKey) {
+  mmap.Insert(1, "value1");
+  mmap.Insert(1, "value2");
+  std::vector<std::string> values = mmap.GetAll(1);
+  EXPECT_EQ(values.size(), 2);
+  EXPECT_EQ(values[0], "value1");
+  EXPECT_EQ(values[1], "value2");
 }
